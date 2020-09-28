5daa3ee1cf0d0b7f453311595f9b94d9253977b4
alesgaber <ales.gaber@gmail.com>
2020-09-28 12:39:20 +0200

    #3

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -54,6 +55,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            //CORS position is important between UseRouting and UseEndPoints
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
