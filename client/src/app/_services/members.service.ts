67d98bd6983af4a9cefea95fbfd936ea2fbdad33
alesgaber <ales.gaber@gmail.com>
2020-10-09 10:16:45 +0200

    #14 UserLikes, Many-many EF

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -97,6 +97,20 @@ export class MembersService {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + 'likes/' + username, {});
+  }
+
+  getLikes(predicate: string, pageNumber, pageSize) {
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('predicate', predicate);
+
+    return this.getPaginatedResult<Partial<Member[]>>(
+      this.baseUrl + 'likes',
+      params
+    );
+  }
+
   private getPaginationHeaders(pageNumber: number, pageSize: number) {
     let params = new HttpParams();
 
@@ -116,7 +130,7 @@ export class MembersService {
           map((response) => {
             pagintedResult.result = response.body;
             if (response.headers.get('Pagination') !== null) {
-              pagintedResult.paginnation = JSON.parse(
+              pagintedResult.pagination = JSON.parse(
                 response.headers.get('Pagination')
               );
             }
